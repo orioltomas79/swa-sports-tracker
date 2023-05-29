876cb6318109f1c0b6d6cd044806f7b0c4c56c95
Oriol Tomas <oriol.tomas@benchmarkcapital.co.uk>
2023-05-29 15:39:43 +0200

    Blob storage

--- api/HttpTrigger1.cs
+++ api/HttpTrigger1.cs
@@ -7,6 +7,11 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Azure.Storage.Blobs;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.VisualBasic;
+using System.Net;
+using System.Text;
 
 namespace Company.Function
 {
@@ -30,6 +35,36 @@ namespace Company.Function
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
+        }
+
+        [FunctionName("Function2")]
+        [OpenApiOperation(operationId: "RunFunction2")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> RunFunction2(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req)
+        {
+            // Create blob client
+            var connectionStr = Environment.GetEnvironmentVariable("StorageAccountConnectionString");
+            const string containerName = "testcontainer";
+            var blobContainerClient = new BlobContainerClient(connectionStr, containerName);
+            await blobContainerClient.CreateIfNotExistsAsync();
+
+            // Get a reference to a blob in a container 
+            const string blobName = "1.json";
+            var blobClient = blobContainerClient.GetBlobClient(blobName);
+
+            // Upload the content
+            var testObj = new TestObject() { TestProperty = DateAndTime.Now.ToLongDateString() };
+            var byteArray = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(testObj));
+            await using var memoryStream = new MemoryStream(byteArray);
+            await blobClient.UploadAsync(memoryStream, overwrite: true);
+
+            return new OkObjectResult("Completed");
+        }
+
+        public class TestObject
+        {
+            public string TestProperty { get; set; }
         }
     }
 }
